fefa811004f6af09ea4ea77bd230369b811f156d
ddabyul <poohjin13@gmail.com>
2020-09-18 13:53:49 +0900

    Updates

---
+++ precache-manifest.718a6217758dab6afb908a279a050611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af0933820420788b80637b69d97dc1e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f8640ab7b4798dc38224",
+    "url": "/movie_app/static/css/main.2a2519a1.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8640ab7b4798dc38224",
+    "url": "/movie_app/static/js/main.4bb99b7b.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
